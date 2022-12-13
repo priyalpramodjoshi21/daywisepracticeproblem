35039d4c9a787f9ce0324ef89cb288b80a7e89ea
“priyalpramodjoshi21” <priyalpramodjoshi@gmail.com>
2022-12-12 17:32:28 -0800

    Added: day4 practice problem added

---
+++ Day4/movefile.sh
@@ -0,0 +1,14 @@
+for file in *.log
+do
+	folderName=`echo $files | awk -F. '{print $1}'`;
+	echo $file;
+        date;
+	echo $folderName;
+	printf "\n";
+	if [ -d $folderName ]
+	then
+		rm -r $folderName;
+	fi
+	mkdir $folderName;
+	cp $file $folderName/$file;
+done
